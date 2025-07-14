3078e7f1d31b7820cb613cb59ca990c80cf83668
Rottschaefer <rottschaefer54@gmail.com>
2025-07-14 20:49:05 -0300

    Criacao da classe de Unidade de controle que vai centralizar todos os sinais e vai ser a fonte para as outras classes

--- Pipeline/MEMWB.java
+++ Pipeline/MEMWB.java
@@ -1,23 +1,5 @@
 package Pipeline;
 
 public class MEMWB {
-    // --- Control Signals ---
-    // WB
-    public static int RegWrite;
-    public static int MemToReg;
-
-    // --- Data ---
-    public static int mem_read_data;
-    public static int alu_result;
-    public static int rd;
-    public static String instructionString;
-
-    public static void flush() {
-        RegWrite = 0;
-        MemToReg = 0;
-        mem_read_data = 0;
-        alu_result = 0;
-        rd = 0;
-        instructionString = "nop";
-    }
+   
 }
---
+++ UnidadeFuncionais/UnidadeDeControle.java
@@ -0,0 +1,11 @@
+package UnidadeFuncionais;
+//Aqui vamos gerir todos os sinais de controle
+//As outras classes vão usar a informação daqui
+
+public class UnidadeDeControle {
+
+    
+    public static void gerarSinais(int opcode) {
+        
+    }
+}
